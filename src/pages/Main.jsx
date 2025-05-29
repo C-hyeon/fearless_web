41e4233f1a40ed2c4b49f6f43fffd9eea004528c
C-hyeon <jung020710@naver.com>
2025-05-29 13:53:19 +0900

    Update Play main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -12,6 +12,20 @@ const Main = () => {
                     <svg className="diagonal-line" viewBox="0 0 100 100" preserveAspectRatio="none" />
                 </div>
             </div>
+            <br/><br/><br/>
+            <div className="main-section">
+                <div className="main-panel">
+                    <h2>게임소개</h2>
+                    <span className="sub">How to play</span>
+                    <div className="number">01</div>
+                </div>
+                <div className="main-background">
+                    <div className="main-overlay">
+                        <h3>예시 소개 글 입니다.</h3>
+                    </div>
+                </div>
+            </div>
+            <br/><br/><br/>
         </div>
     );
 };
