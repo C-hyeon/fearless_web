c12bbbb0ba970662d197cb679b6944c4d082936e
C-hyeon <jung020710@naver.com>
2025-06-19 05:09:57 +0900

    framer-motion Update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,27 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import "../styles/Modal.scss";
 
 const Modal = ({isOpen, onClose, children}) => {
-    if(!isOpen) return null;
-
     return (
-        <div className="overlay" onClick={onClose}>
-            <motion.div 
-                className="modal" 
-                onClick={(e)=>e.stopPropagation()}
-                initial={{opacity: 0, y: -50}}
-                animate={{opacity: 1, y: 0}}
-                exit={{opacity: 0, y: 50}}
-                transition={{duration: 0.3}}
-            >
-                <button className="closeButton" onClick={onClose}>
-                    &times;
-                </button>
-                {children}
-            </motion.div>
-        </div>
+        <AnimatePresence>
+            {isOpen && (
+                <div className="overlay" onClick={onClose}>
+                    <motion.div 
+                        className="modal" 
+                        onClick={(e)=>e.stopPropagation()}
+                        initial={{opacity: 0, y: -50}}
+                        animate={{opacity: 1, y: 0}}
+                        exit={{opacity: 0, y: 50}}
+                        transition={{duration: 0.4}}
+                    >
+                        <button className="closeButton" onClick={onClose}>
+                            &times;
+                        </button>
+                        {children}
+                    </motion.div>
+                </div>
+            )}
+        </AnimatePresence>
     );
 };
 
