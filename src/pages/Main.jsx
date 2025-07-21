7511baece42ac107015c58f1471154badd7c4d35
C-hyeon <jung020710@naver.com>
2025-07-21 11:31:57 +0900

    Main 페이지 이미지 슬라이더 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaChevronRight } from 'react-icons/fa';
 import { AnimatePresence, motion } from 'framer-motion';
-
 import Wrapper from '../components/Wrapper';
 import "../styles/Main.scss";
+
+const stageImages = [
+    "https://firebasestorage.googleapis.com/v0/b/fearless-3e591.firebasestorage.app/o/stages%2FStage0_TrainingRoom.png?alt=media&token=4f42eb9b-b58d-40be-987a-b5bd56159a0a",
+    "https://firebasestorage.googleapis.com/v0/b/fearless-3e591.firebasestorage.app/o/stages%2FStage1_RobotFactory.png?alt=media&token=b3af8817-61b3-4590-8415-d0b73532bae1",
+    "https://firebasestorage.googleapis.com/v0/b/fearless-3e591.firebasestorage.app/o/stages%2FStage2_IncubatorRoom.jpg?alt=media&token=05e0f8ff-800f-41eb-8be8-fc2341d289d3",
+    "https://firebasestorage.googleapis.com/v0/b/fearless-3e591.firebasestorage.app/o/stages%2FStage3_GeneratorRoom.jpg?alt=media&token=7ef768c3-9c69-4090-b1a6-cf7c24170879",
+    "https://firebasestorage.googleapis.com/v0/b/fearless-3e591.firebasestorage.app/o/stages%2FStage4_RuinedCity.png?alt=media&token=9a4bade5-3089-40df-8722-f3d5fadf6ac8"
+];
 
 const Play_Menu = () => {
     return (
@@ -72,27 +79,23 @@ const Store_Menu = () => {
 const Main = () => {
     const [menu, setMenu] = useState('play');
     const [prevMenu, setPrevMenu] = useState('play');
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    const menuOrder = ['play', 'event', 'store'];
-    const direction = menuOrder.indexOf(menu) > menuOrder.indexOf(prevMenu) ? 1 : -1;
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex(prev => (prev + 1) % stageImages.length);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const direction = ['play', 'event', 'store'].indexOf(menu) > ['play', 'event', 'store'].indexOf(prevMenu) ? 1 : -1;
 
     const renderMenu = () => {
         let Component = null;
         if (menu === 'play') Component = <Play_Menu/>;
         if (menu === 'event') Component = <Event_Menu/>;
         if (menu === 'store') Component = <Store_Menu/>;
-
-        return (
-            <motion.div
-                key={menu}
-                initial={{ x: 100 * direction, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                exit={{ x: -100 * direction, opacity: 0 }}
-                transition={{ duration: 0.3 }}
-            >
-                {Component}
-            </motion.div>
-        );
+        return Component;
     };
 
     const handleClick = (newMenu) => {
@@ -110,7 +113,22 @@ const Main = () => {
                         <div className="left-panel">
                             <h1>Fear Less</h1>
                         </div>
-                        <div className="right-panel" />
+                        <AnimatePresence mode="wait">
+                            {stageImages.map((url, index) =>
+                                index === currentImageIndex && (
+                                    <motion.div
+                                        key={url}
+                                        className="right-panel"
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        transition={{ duration: 1 }}
+                                        style={{ backgroundImage: `url(${url})` }}
+                                    />
+                                )
+                            )}
+                        </AnimatePresence>
+
                         <svg className="diagonal-line" viewBox="0 0 100 100" preserveAspectRatio="none" />
                     </div>
                 </div>
@@ -122,7 +140,15 @@ const Main = () => {
                 </div>
                 <br/>
                 <AnimatePresence mode="wait">
-                    {renderMenu()}
+                    <motion.div
+                        key={menu}
+                        initial={{ x: 100 * direction, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        exit={{ x: -100 * direction, opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        {renderMenu()}
+                    </motion.div>
                 </AnimatePresence>
                 <br/><br/><br/>
             </div>
